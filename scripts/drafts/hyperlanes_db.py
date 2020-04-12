bccc9279cd3036b50ead7959d2ab2f6e31f6275f
Wladymir Brborich <wabrborich@hotmail.com>
2020-04-11 23:28:44 -0500

    add hyperlanes as graph edges

--- scripts/drafts/hyperlanes_db.py
+++ scripts/drafts/hyperlanes_db.py
@@ -1,13 +1,9 @@
 
-
-route_name = 'Rimma Trade Route'
-
-raw_planets = "Abregado-rae[1] - Dentaal[1] - Giju[1] - Ghorman[1] - Vanik[1] - Thyferra[1] - Tauber[1] - Yag'Dhul[1] - Sukkult[1] - Wroona[1] - " +\
-    "Tregillis[1] - Vandelhelm[1] - Woostri[1] - Daemen[1] - Alakatha[1] - Lanthe[1] - Vondarc[1] - Medth[1] - Tshindral III[1] - Sullust[1] - Eriadu[1] - " +\
-    "Clak'dor VII[1] - Triton[1] - Praesitlyn/Sluis Van[1] - Denab[1] - Tarabba Prime[1] - Adarlon[1] - Karideph[1]"
-
-planets = [planet.strip('[1]') for planet in raw_planets.split(' - ')]
-
-print(planets)
-
-# junctions = []
+{
+    'Rimma Trade Route': ['Abregado-rae', 'Dentaal', 'Giju',
+                          'Ghorman', 'Vanik', 'Thyferra', 'Tauber', "Yag'Dhul", 'Sukkult',
+                          'Wroona', 'Tregillis', 'Vandelhelm', 'Woostri', 'Daemen',
+                          'Alakatha', 'Lanthe', 'Vondarc', 'Medth', 'Tshindral III',
+                          'Sullust', 'Eriadu', "Clak'dor VII", 'Triton', 'Praesitlyn', 'Sluis Van',
+                          'Denab', 'Tarabba Prime', 'Adarlon', 'Karideph']
+}
--- scripts/graph/transform_db_to_graph.py
+++ scripts/graph/transform_db_to_graph.py
@@ -8,10 +8,27 @@ import json
 import networkx as nx
 import matplotlib.pyplot as plt
 
+HYPERLANES = {
+    'Rimma Trade Route': ['Abregado-rae', 'Dentaal', 'Giju',
+                          'Ghorman', 'Vanik', 'Thyferra', 'Tauber', "Yag'Dhul", 'Sukkult',
+                          'Wroona', 'Tregillis', 'Vandelhelm', 'Woostri', 'Daemen',
+                          'Alakatha', 'Lanthe', 'Vondarc', 'Medth', 'Tshindral',
+                          'Sullust', 'Eriadu', 'Bith', 'Triton', 'Praesitlyn', 'Sluis Van',
+                          'Denab', 'Tarabba', 'Adarlon', 'Karideph', 'Pergitor', "Kal'Shebbol"],
+
+    'Corellinan Run': ['Coruscant', 'Ixtlar', 'Corellia', 'Tinnel', 'Loronar', 'Byblos', 'Iseno',
+                       'Denon', 'Spirana', 'Rhommamool', 'Allanteen', 'Gamor', 'Milagro', 'New Cov',
+                       'Druckenwell', 'Mon Gazza', 'Herdessa', 'Radnor', 'Christophsis', 'Savareen',
+                       'Ryloth', "Smuggler's Run"],
+
+    'Corellian Trade Spine': ['Corellia', 'Hosnian Prime', 'Kelada', 'Foless', 'Mechis III', "Yag'Dhul", 'Harrin', 'Kinyen', 'Bomis Koori', 'Kriselist', 'Kaal', 'Jiroch', 'Mugaar', 'Gerrenthum',
+                              'Isde Naha', "Berrol's Donn", 'Manpha', 'Terminus']
+}
+
 
 def plot_graph(graph, node_color=None):
     pos = {node: list(node.location.coords)[0] for node in graph.nodes()}
-    nx.draw_networkx(graph, pos=pos, node_size=10, with_labels=False, node_color=node_color, font_size=8)
+    nx.draw_networkx(graph, pos=pos, node_size=10, with_labels=False, node_color=node_color)
 
 
 def get_region_db(db_path) -> List['Region']:
@@ -32,15 +49,32 @@ def get_grid_db(db_path) -> List['Grid']:
             for grid, planets in db.items()]
 
 
+def get_planet_pair(planet_list: list):
+    return list(zip(*[planet_list[index::1] for index in range(2)]))
+
+
 if __name__ == "__main__":
     region_path = 'D:/wason/Documents/Trabajos Universidad/Proyectos y Soluciones/StarWarsMap/data/regions_db.json'
     grid_path = 'D:/wason/Documents/Trabajos Universidad/Proyectos y Soluciones/StarWarsMap/data/grid_db.json'
 
     regions = get_region_db(region_path)
     grids = get_grid_db(grid_path)
+    planet_list = list(chain.from_iterable(region.planets for region in regions))
+
+    planet_search_dict = {planet.name: planet for planet in planet_list}
 
     graph = nx.Graph()
-    graph.add_nodes_from(chain.from_iterable(region.planets for region in regions))
+    graph.add_nodes_from(planet_list)
+
+    for route, planets in HYPERLANES.items():
+        planet_routes = get_planet_pair(planets)
+        edges = [(planet_search_dict[start],
+                  planet_search_dict[end],
+                  planet_search_dict[start].location.distance(planet_search_dict[end].location))
+                 for start, end in planet_routes]
+
+        graph.add_weighted_edges_from(edges, label=route)
+
     plot_graph(graph)
 
     for region in regions:
@@ -50,6 +84,9 @@ if __name__ == "__main__":
     for grid in grids:
         if isinstance(grid.shape, Polygon):
             plt.plot(*grid.shape.exterior.xy)
+
+    # for planet in nx.isolates(graph):
+    #     plt.plot(*planet.location.coords)
 
     # for sector in chain.from_iterable(region.sectors for region in regions):
     #     shape = sector.shape.exterior.xy if isinstance(sector.shape, Polygon) else sector.shape.xy
